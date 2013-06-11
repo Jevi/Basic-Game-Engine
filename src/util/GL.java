33c868c7076e0918667eda8370097490ce78c2db
jevi <arnunezm@yahoo.com>
2013-06-11 13:26:18 -0500

    fixed inconsistency in GL.checkShaderInfo

--- src/util/GL.java
+++ src/util/GL.java
@@ -1,9 +1,8 @@
 package util;
 
-import static org.lwjgl.opengl.GL11.GL_NO_ERROR;
-import static org.lwjgl.opengl.GL11.glGetError;
-import static org.lwjgl.opengl.GL20.glGetShaderInfoLog;
-import static org.lwjgl.util.glu.GLU.gluErrorString;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.util.glu.GLU.*;
 
 public class GL {
 
@@ -16,7 +15,7 @@ public class GL {
 
 	public static void checkShaderInfo(int id) throws GLException {
 		String s = glGetShaderInfoLog(id, 1000);
-		if (!s.isEmpty()) {
+		if (!s.trim().contains("No errors.")) {
 			throw new GLException(s);
 		}
 	}
